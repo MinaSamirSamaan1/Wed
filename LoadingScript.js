4a96669edde3fe77b449bc25fcc6e6a0bb2aaf3d
Mina <mina.samir@ibm.com>
2019-11-17 10:09:19 +0200

    add all

---
+++ LoadingScript.js
@@ -0,0 +1,15 @@
+function AsIs()
+{
+  setTimeout(function(){
+
+     document.getElementById("mina-img").classList.remove('to-be-retrieved');
+     document.getElementById('mina-spinner').style.display="none";
+
+
+
+},1000)
+};
+
+function slide(){
+    document.getElementById('mina-slide').className ='slide';
+  };
---
+++ README.md
@@ -0,0 +1,7 @@
+
+ - GIT PAGES URL: https://minasamirsamaan1.github.io/AngularJS/
+
+ - Useful Commands: 
+ 
+ -- browser-sync start --server --directory --files "**/*" 
+ 
